c79722726b8c98c422eefcdb70c4983e2ab2b6ef
Matthaia <matthias97@live.nl>
2020-12-13 00:13:21 +0100

    Day11 complete

--- CScherp/AdventOfCode2020/AdventOfCode2020/Days/Day11.cs
+++ CScherp/AdventOfCode2020/AdventOfCode2020/Days/Day11.cs
@@ -1,19 +1,32 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 namespace AdventOfCode2020.Days
 {
-	public class Day11
+	public enum Direction
+	{
+		Top,
+		TopRight,
+		Right,
+		BottomRight,
+		Bottom,
+		BottomLeft,
+		Left,
+		TopLeft
+	}
+
+	public class Day11Airplane
 	{
 		public char[][] Seats { get; set; }
 
-		public Day11()
+		public Day11Airplane(string[] rows)
 		{
-			var rawInput = FileReader.ReadAllLines("Day11.txt");
+			Seats = new char[rows.Length][];
 
-			Seats = new char[rawInput.Length][];
-
-			for(int y = 0; y < rawInput.Length; y++)
+			for (int y = 0; y < rows.Length; y++)
 			{
-				var seats = rawInput[y].ToCharArray();
+				var seats = rows[y].ToCharArray();
 				Seats[y] = new char[seats.Length];
 
 				for (int x = 0; x < seats.Length; x++)
@@ -23,17 +36,186 @@ namespace AdventOfCode2020.Days
 			}
 		}
 
-		public int NumberOfAdjecentOccupiedSeats
+		public Day11Airplane(char[][] seats)
+		{
+			Seats = seats;
+		}
+
+		public int NumberOfAdjecentOccupiedSeats(int x, int y, bool withJumps = false)
+		{
+			var occupiedPlaces = 0;
+
+			foreach(var direction in (Direction[])Enum.GetValues(typeof(Direction)))
+			{
+				var seat = GetNextSeat(x, y, direction, withJumps);
+
+				if (seat.HasValue && seat.Value == '#')
+					occupiedPlaces++;
+			}
+
+			return occupiedPlaces;
+		}
+
+		public char? GetNextSeat(int x, int y, Direction direction, bool withJumps = false)
+		{
+			var directionData = new Dictionary<Direction, (int yDiff, int xDiff)>
+			{
+				{ Direction.Top, (-1, 0) },
+				{ Direction.TopRight, (-1, 1) },
+				{ Direction.Right, (0, 1) },
+				{ Direction.BottomRight, (1, 1) },
+				{ Direction.Bottom, (1, 0) },
+				{ Direction.BottomLeft, (1, -1) },
+				{ Direction.Left, (0, -1) },
+				{ Direction.TopLeft, (-1, -1) }
+			};
+
+			var directionDiff = directionData[direction];
+
+			var (X, Y) = (directionDiff.xDiff + x, directionDiff.yDiff + y);
+
+			while(true)
+			{
+				if (Y < 0 || Y >= Seats.Length)
+					return null;
+
+				if (X < 0 || X >= Seats[Y].Length)
+					return null;
+
+				if (!withJumps)
+					return Seats[Y][X];
+
+				if (Seats[Y][X] == '#')
+					return '#';
+
+				if (Seats[Y][X] == 'L')
+					return null;
+
+				X += directionDiff.xDiff;
+				Y += directionDiff.yDiff;
+			}
+		}
+
+		public (int changes, char[][] seats) Excercise1()
+		{
+			var newMap = GetCopy();
+			var changes = 0;
+			for (int y = 0; y < Seats.Length; y++)
+			{
+				for (int x = 0; x < Seats[y].Length; x++)
+				{
+					var seat = Seats[y][x];
+					switch (seat)
+					{
+						case 'L':
+							if (NumberOfAdjecentOccupiedSeats(x, y) == 0)
+							{
+								newMap[y][x] = '#';
+								changes++;
+							}
+							break;
+
+						case '#':
+							if (NumberOfAdjecentOccupiedSeats(x, y) >= 4)
+							{
+								newMap[y][x] = 'L';
+								changes++;
+							}
+							break;
+					}
+				}
+			}
+
+			return (
+				changes,
+				newMap
+			);
+		}
+
+		public (int changes, char[][] seats) Excercise2()
+		{
+			var newMap = GetCopy();
+			var changes = 0;
+			for (int y = 0; y < Seats.Length; y++)
+			{
+				for (int x = 0; x < Seats[y].Length; x++)
+				{
+					var seat = Seats[y][x];
+					switch (seat)
+					{
+						case 'L':
+							if (NumberOfAdjecentOccupiedSeats(x, y, true) == 0)
+							{
+								newMap[y][x] = '#';
+								changes++;
+							}
+							break;
+
+						case '#':
+							if (NumberOfAdjecentOccupiedSeats(x, y, true) >= 5)
+							{
+								newMap[y][x] = 'L';
+								changes++;
+							}
+							break;
+					}
+				}
+			}
+
+			return (
+				changes,
+				newMap
+			);
+		}
+
+		public char[][] GetCopy() => new Day11Airplane(Seats.Select(x => string.Join("", x)).ToArray()).Seats;
 
 		public override string ToString()
 		{
 			var builder = new StringBuilder();
 			foreach (var row in Seats)
 			{
-				builder.Append($"[{string.Join(",", row)}]");
+				builder.Append($"[{string.Join("", row)}]");
 			}
 
 			return builder.ToString();
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj as Day11Airplane == null)
+				return false;
+
+			var other = obj as Day11Airplane;
+
+			if (other.Seats.Length != Seats.Length)
+				return false;
+
+			for (int y = 0; y < Seats.Length; y++)
+			{
+				if (other.Seats[y].Length != Seats[y].Length)
+					return false;
+
+				for (int x = 0; x < Seats[y].Length; x++)
+				{
+					if (other.Seats[y][x] != Seats[y][x])
+						return false;
+				}
+			}
+
+			return true;
+		}
+	}
+
+	public class Day11
+	{
+		public Day11Airplane Plane { get; set; }
+
+		public Day11()
+		{
+			var rawInput = FileReader.ReadAllLines("Day11.txt");
+
+			Plane = new Day11Airplane(rawInput);
 		}
 	}
 }
--- CScherp/AdventOfCode2020/UnitTests/Days/Day11Test.cs
+++ CScherp/AdventOfCode2020/UnitTests/Days/Day11Test.cs
@@ -1,19 +1,192 @@
 ﻿using AdventOfCode2020.Days;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 using Xunit;
 
 namespace UnitTests.Days
 {
 	public class Day11Test
 	{
+		private const string TestFilesPath = "../../../Input/Day11/";
+
 		[Fact]
 		public void SetupCorrectly()
 		{
 			var day = new Day11();
 
-			Assert.True(day.Seats.Length > 0);
+			Assert.True(day.Plane.Seats.Length > 0);
+		}
+
+		[Theory]
+		[InlineData(0, 0, 2)]
+		[InlineData(0, 1, 3)]
+		[InlineData(0, 2, 3)]
+		[InlineData(1, 0, 4)]
+		[InlineData(1, 1, 6)]
+		[InlineData(1, 2, 5)]
+		[InlineData(2, 0, 2)]
+		[InlineData(2, 1, 4)]
+		[InlineData(2, 2, 3)]
+		public void NumberOfAdjecentOccupiedSeatsTest(int y, int x, int expected)
+		{
+			var airplane = new Day11Airplane(new string[] {
+				".##",
+				"L##",
+				"###"
+			});
+
+			var result = airplane.NumberOfAdjecentOccupiedSeats(x, y);
+
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void CopyTest()
+		{
+			var input = @"L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL";
+
+			var airplane = new Day11Airplane(input.Split("\r\n"));
+
+			var result = new Day11Airplane(airplane.GetCopy());
+
+			Assert.True(airplane.Equals(result));
+			Assert.True(result.Equals(airplane));
+		}
+
+
+		[Theory]
+		[InlineData("Fase1.txt", "Fase2.txt", 71)]
+		[InlineData("Fase2.txt", "Fase3.txt", 51)]
+		[InlineData("Fase3.txt", "Fase4.txt", 31)]
+		[InlineData("Fase4.txt", "Fase5.txt", 21)]
+		[InlineData("Fase5.txt", "Fase6.txt", 7)]
+		[InlineData("Fase6.txt", "Fase7.txt", 0)]
+		public void Exercise1SmallTest(string start, string end, int expectedChanges)
+		{
+			var plane = new Day11Airplane(File.ReadAllLines($"{TestFilesPath}{start}"));
+			var (changes, seats) = plane.Excercise1();
+
+			var expected = new Day11Airplane(File.ReadAllLines($"{TestFilesPath}{end}"));
+			var actual = new Day11Airplane(seats);
+
+			Assert.Equal(expected, actual);
+			Assert.Equal(expectedChanges, changes);
+		}
+
+		[Fact]
+		public void Exercise1Test()
+		{
+			var plane = new Day11().Plane;
+			var (changes, seats) = plane.Excercise1();
+			var history = new List<(int changes, char[][] seats)> { (0, plane.Seats) };
+
+			while (changes > 0)
+			{
+				history.Add((changes, seats));
+				plane = new Day11Airplane(seats);
+				(changes, seats) = plane.Excercise1();
+			}
+
+			var filledSeats = 0;
+			for (int y = 0; y < seats.Length; y++)
+			{
+				for (int x = 0; x < seats[y].Length; x++)
+				{
+					if (seats[y][x] == '#') filledSeats++;
+				}
+			}
+
+			Assert.Equal(2468, filledSeats);
+		}
+
+		[Fact]
+		public void NumberOfAdjecentOccupiedSeatsWithJumpsTest_0()
+		{
+			var input = @".##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##.".Split("\r\n");
+
+			var plane = new Day11Airplane(input);
+
+			var result = plane.NumberOfAdjecentOccupiedSeats(3, 3, true);
+
+			Assert.Equal(0, result);
+		}
+
+
+		[Fact]
+		public void NumberOfAdjecentOccupiedSeatsWithJumpsTest_8()
+		{
+			var input = @".......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....".Split("\r\n");
+
+			var plane = new Day11Airplane(input);
+
+			var result = plane.NumberOfAdjecentOccupiedSeats(3, 4, true);
+
+			Assert.Equal(8, result);
+		}
+
+		[Fact]
+		public void NumberOfAdjecentOccupiedSeatsWithJumpsTest_1()
+		{
+			var input = @".............
+.L.L.#.#.#.#.
+.............".Split("\r\n");
+
+			var plane = new Day11Airplane(input);
+
+			var result = plane.NumberOfAdjecentOccupiedSeats(3, 4, true);
+
+			Assert.Equal(0, result);
+		}
+
+
+
+		[Fact]
+		public void Exercise2Test()
+		{
+			var plane = new Day11().Plane;
+			var (changes, seats) = plane.Excercise2();
+			var history = new List<(int changes, char[][] seats)> { (0, plane.Seats) };
+
+			while (changes > 0)
+			{
+				history.Add((changes, seats));
+				plane = new Day11Airplane(seats);
+				(changes, seats) = plane.Excercise2();
+			}
+
+			var filledSeats = 0;
+			for (int y = 0; y < seats.Length; y++)
+			{
+				for (int x = 0; x < seats[y].Length; x++)
+				{
+					if (seats[y][x] == '#') filledSeats++;
+				}
+			}
+
+			Assert.Equal(2214, filledSeats);
 		}
 	}
 }
