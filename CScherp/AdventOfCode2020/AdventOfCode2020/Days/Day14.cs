d4c182be7d39e8ab295174ae81515271b5da9086
Matthaia <matthias97@live.nl>
2020-12-14 21:06:32 +0100

    Day 14

---
+++ CScherp/AdventOfCode2020/AdventOfCode2020/Days/Day14.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Days
+{
+	public class MemoryValue
+	{
+		public char[] Bits { get; set; }
+		public MemoryValue()
+		{
+			Bits = Enumerable.Repeat('0', 36).ToArray();
+		}
+
+		public MemoryValue(string value)
+		{
+			Bits = value.Split(" = ")[1].ToCharArray();
+		}
+
+		public MemoryValue(long value)
+		{
+			Bits = Convert.ToString(value, 2).PadLeft(36, '0').ToCharArray();
+		}
+
+		public MemoryValue Mask(MemoryValue mask)
+		{
+			var result = new MemoryValue();
+
+			for (int i = 0; i < 36; i++)
+			{
+				result.Bits[i] = mask.Bits[i] == 'X' ? Bits[i] : mask.Bits[i];
+			}
+
+			return result;
+		}
+
+		public MemoryValue MaskV2(MemoryValue mask)
+		{
+			var result = new MemoryValue();
+
+			for (int i = 0; i < 36; i++)
+			{
+				if (mask.Bits[i] == '0')
+					result.Bits[i] = Bits[i];
+				else
+					result.Bits[i] = mask.Bits[i];
+			}
+
+			return result;
+		}
+
+		public IList<long> GetMemoryAddresses(MemoryValue mask)
+		{
+			var results = new List<string> { "" };
+
+			var masked = MaskV2(mask);
+
+			for (int i = 0; i < 36; i++)
+			{
+				var bit = masked.Bits[i];
+
+				if (bit != 'X')
+					for (int y = 0; y < results.Count; y++)
+						results[y] += bit;
+				else
+				{
+					var backupResults = results.Select(x => x += '1').ToList();
+
+					for (int y = 0; y < results.Count; y++)
+						results[y] += '0';
+
+					foreach (var result in backupResults)
+						results.Add(result);
+				}
+			}
+
+			return results.Select(x => Convert.ToInt64(x, 2)).OrderBy(x => x).ToList();
+		}
+
+		public long ToLong()
+		{
+			return Convert.ToInt64(string.Join("", Bits), 2);
+		}
+	}
+
+	public class Day14
+	{
+		public string[] Lines { get; set; }
+
+		public Day14() : this(FileReader.ReadAllLines("Day14.txt")) { }
+
+		public Day14(string[] lines)
+		{
+			Lines = lines;
+		}
+
+		public long Exercise1()
+		{
+			var mask = new MemoryValue(Lines[0]);
+			var memory = new Dictionary<int, MemoryValue>();
+
+			for (int i = 1; i < Lines.Length; i++)
+			{
+				var line = Lines[i];
+
+				if (line.StartsWith("mask"))
+				{
+					mask = new MemoryValue(line);
+					continue;
+				}
+
+				var address = int.Parse(line.Split('[', ']')[1]);
+
+				memory[address] = new MemoryValue(long.Parse(line.Split(" = ")[1])).Mask(mask);
+			}
+
+			return memory.Values.Sum(x => x.ToLong());
+		}
+
+		public long Exercise2()
+		{
+			var mask = new MemoryValue(Lines[0]);
+			var memory = new Dictionary<long, long>();
+
+			for (int i = 1; i < Lines.Length; i++)
+			{
+				var line = Lines[i];
+
+				if (line.StartsWith("mask"))
+				{
+					mask = new MemoryValue(line);
+					continue;
+				}
+
+				var address = int.Parse(line.Split('[', ']')[1]);
+				var value = long.Parse(line.Split(" = ")[1]);
+				var addresses = new MemoryValue(address).GetMemoryAddresses(mask);
+
+				foreach (var item in addresses)
+				{
+					memory[item] = value;
+				}
+			}
+
+			return memory.Values.Sum(x => x);
+		}
+	}
+}
---
+++ CScherp/AdventOfCode2020/UnitTests/Days/Day14Test.cs
@@ -0,0 +1,88 @@
+﻿using AdventOfCode2020.Days;
+using Xunit;
+
+namespace UnitTests.Days
+{
+	public class Day14Test
+	{
+		[Fact]
+		public void Exercise1Test()
+		{
+			var day = new Day14();
+
+			var result = day.Exercise1();
+
+			Assert.Equal(15919415426101, result);
+		}
+
+		[Theory]
+		[InlineData(1577116)]
+		[InlineData(3562548)]
+		[InlineData(60801469)]
+		[InlineData(974)]
+		[InlineData(61211605)]
+		[InlineData(662806)]
+		[InlineData(120026)]
+		public void ToLongTest(long value)
+		{
+			var memory = new MemoryValue(value);
+
+			Assert.Equal(value, memory.ToLong());
+		}
+
+		[Fact]
+		public void Exercise2_Short()
+		{
+			var input = @"mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1".Split("\r\n");
+
+			var day = new Day14(input);
+
+			var result = day.Exercise2();
+
+			Assert.Equal(208, result);
+		}
+
+		[Theory]
+		[InlineData("0000000X0000000000000X00000000X1001X", 16)]
+		[InlineData("00000000000000000000000000000000X0XX", 8)]
+		[InlineData("000000000000000000000000000000X1001X", 4)]
+		[InlineData("000000000000000000000000000000X10010", 2)]
+		[InlineData("000000000000000000000000000000010010", 1)]
+		public void GetMemoryAddressesTest(string mask, int expected)
+		{
+			var memoryValue = new MemoryValue(0);
+			var maskValue = new MemoryValue(" = " + mask);
+
+			var result = memoryValue.GetMemoryAddresses(maskValue);
+
+			Assert.Equal(expected, result.Count);
+		}
+
+		[Fact]
+		public void GetMemoryAddressesTest_CorrectLong()
+		{
+			var memoryValue = new MemoryValue(42);
+			var maskValue = new MemoryValue(" = 000000000000000000000000000000X1001X");
+
+			var result = memoryValue.GetMemoryAddresses(maskValue);
+
+			Assert.Equal(26, result[0]);
+			Assert.Equal(27, result[1]);
+			Assert.Equal(58, result[2]);
+			Assert.Equal(59, result[3]);
+		}
+
+		[Fact]
+		public void Exercise2Test()
+		{
+			var day = new Day14();
+
+			var result = day.Exercise2();
+
+			Assert.Equal(3443997590975, result);
+		}
+	}
+}
